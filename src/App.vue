5fa11e1ca9b7908833a49544de782553155ed1e4
徐金俊 <939160741@qq.com>
2021-05-21 16:28:59 +0800

    feat(xjj): init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hi</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
