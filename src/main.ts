801084c5db4b765d3aad246c7b4ceabe961b5b3b
徐金俊 <939160741@qq.com>
2021-05-25 10:22:44 +0800

    feat(xjj): 样式更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import {createWebHashHistory, createRouter} from 'vue-router'
 import Home from './view/Home.vue'
 import Doc from './view/Doc.vue'
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,20 +1,38 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
-      <h1>ui</h1>
-      <h2>xxxxxx</h2>
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a href="">1</a>
-        <a href="">2</a>
+        <a>GitHub</a>
+        <a>开始</a>
       </p>
     </div>
   </div>
 </template>
-
 <style lang="scss" scoped>
-
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
