6a62a847181881cc5622b07aa546953e45211f97
徐金俊 <939160741@qq.com>
2021-05-27 14:33:48 +0800

    feat(xjj): switch 基础功能

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,19 @@
 <template>
-  <Switch value="false"/>
+  <Switch :value="false" @input="y = $event"/>
 </template>
 
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
+import { ref } from 'vue'
 export default {
   name: 'switchDemo',
   components: {
     Switch
   },
   setup () {
+    const y = ref(true)
     return {
+      y
     }
   }
 }
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,18 +1,20 @@
 <template>
-  <button :class="{checked}" @click="toggle"> <span></span> </button>
+  <button :class="{checked: value}" @click="toggle"> <span></span> </button>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
 
 export default {
-  setup () {
-    let checked = ref(false)
+  props: {
+    value: {
+      type: Boolean || String
+    }
+  },
+  setup (props, context) {
     const toggle = () => {
-      checked.value = !checked.value
+      context.$emit('input', !props.value)
     }
     return {
-      checked,
       toggle
     }
   }
@@ -38,11 +40,15 @@ span{
   width: $h2;
   background:white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 button.checked{
   background: blue;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
 }
 </style>
