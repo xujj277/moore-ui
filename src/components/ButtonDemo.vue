188902e0f871f216f8dd8177456b34c929ec02bb
徐金俊 <939160741@qq.com>
2021-05-28 16:39:48 +0800

    feat(xjj): button基础功能

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,22 +1,69 @@
 <template>
-  <x-button @click="xxx" @focus="xxx" @mouseover="xxx" :theme="'large'">xxx</x-button>
-  <x-button @click="xxx" @focus="xxx" @mouseover="xxx" :theme="'large'">xxx</x-button>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
+  <h1>示例2</h1>
+  <div>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>普普通</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">普普通</Button>
+      <Button size="small" theme="link">小小小</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大大大</Button>
+      <Button theme="text">普普通</Button>
+      <Button size="small" theme="text">小小小</Button>
+    </div>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <dir>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </dir>
+  </div>
+  <h1>示例4</h1>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
+  </div>
 </template>
-
 <script lang="ts">
-import xButton from '../lib/Button.vue'
+import Button from "../lib/Button.vue";
 export default {
-  components: {xButton},
-  setup () {
-    const xxx = () => {
-      console.log(2222)
-    }
-    return {
-      xxx
-    }
-  }
-}
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
+};
 </script>
-
-<style lang='scss' scoped>
-</style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,22 +1,48 @@
 <template>
-  <button class="x-button" :class="{[`x-theme-${theme}`]: theme}"><slot></slot></button>
+  <button class="x-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="x-loadingIndicator"></span>
+    <slot></slot>
+  </button>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue'
+
 export default {
   inheritAttrs: false,
   props: {
     theme: {
       type: String,
       default: 'button'
-    }
+    },
+    level: {
+      type: String,
+      default: 'normal'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
   },
   setup (props, context) {
-    const {size, ...rest} = context.attrs
-    console.log(size)
+    const {theme, level, size} = props
+    const classes = computed(() => {
+      return {
+        [`x-theme-${theme}`]: theme,
+        [`x-level-${level}`]: level,
+        [`x-size-${size}`]: size,
+      }
+    })
     return {
-      size,
-      rest
+      classes
     }
   }
 }
@@ -28,6 +54,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .x-button {
   box-sizing: border-box;
   height: $h;
@@ -42,6 +70,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -56,5 +85,110 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.x-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.x-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.x-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.x-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.x-theme-button {
+    &.x-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.x-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.x-theme-link {
+    &.x-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.x-theme-text {
+    &.x-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.x-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.x-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.x-theme-link, &.x-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .x-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: x-spin 1s infinite linear;
+  }
+}
+@keyframes x-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
