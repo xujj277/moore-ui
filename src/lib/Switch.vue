eae380cf77721d77f5ad284db19c8013e44e455d
徐金俊 <939160741@qq.com>
2021-05-28 15:13:38 +0800

    feat(xjj): css最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="{checked: value}" @click="toggle"><span></span></button>
+  <button class="x-switch" :class="{'x-checked': value}" @click="toggle"><span></span></button>
 </template>
 
 <script lang="ts">
@@ -24,35 +24,35 @@ export default {
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button {
+.x-switch {
   height: $h;
   width: $h*2;
   border: none;
   background: grey;
   border-radius: $h/2;
   position: relative;
-}
 
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
-}
-
-button.checked {
-  background: blue;
-}
-
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-
-button:focus {
-  outline: none;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 250ms;
+  }
+  
+  &.x-checked {
+    background: blue;
+  }
+  
+  &.x-checked > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+  
+  &:focus {
+    outline: none;
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import './lib/x.scss'
+import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.scss'
 import { router } from './router'
 
 const app = createApp(App)
