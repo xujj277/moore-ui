f847d55941e6e765e505955949dd59b65cd7454e
徐金俊 <939160741@qq.com>
2021-06-22 16:44:49 +0800

    feat(xjj): 首页基本样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-zhuangyuan"></use>
+      </svg>
+    </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <router-link to="/doc">文档</router-link>
     </ul>
     <span class="toggleAside" @click="toggleMenu"></span>
   </div>
@@ -25,7 +28,6 @@ export default {
 </script>
 <style lang="scss" scoped>
 .topnav {
-  background: pink;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -38,6 +40,12 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    
+    >svg {
+      width: 32px;
+      height: 32px;
+      fill: #e7977b;
+    }
   }
   > .menu {
     display: flex;
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
-    <Topnav/>
-    <div class="banner">
-      <h1>轱辘UI</h1>
-      <h2>一个厉害的 UI 框架</h2>
-      <p class="actions">
-        <a href="https://github.com/xujj277/xu-ui-1">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+    <div class="home-page">
+      <Topnav/>
+      <div class="banner">
+        <h1>轱辘UI</h1>
+        <h2>一个厉害的 UI 框架</h2>
+        <p class="actions">
+          <a href="https://github.com/xujj277/xu-ui-1">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h2>基于 Vue 3</h2>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-typescript"></use>
+          </svg>
+          <h2>基于 TypeScript </h2>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h2>代码易读</h2>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -23,24 +50,73 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$moBgColor: #e7977b;
+$border-radius: 4px;
+$color: #c66f48;
+
+.home-page {
+  background: linear-gradient(145deg, rgba(245,235,224,1) 0%, rgba(229,211,196,1) 49%, rgba(244,234,223,1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
+}
+.features {
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h2 {
+        grid-area: title;
+        font-size: 28px;
+        color: #885345;
+      }
+      >p {
+        grid-area: text;
+        color: #c08364;
+      }
+    }
+  }
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
   > .actions{
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $moBgColor;
+      color: white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
