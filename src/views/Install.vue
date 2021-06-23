1166f1e61a566c76f9fee1e0e59a1db2c70c2d04
徐金俊 <939160741@qq.com>
2021-06-23 14:53:53 +0800

    feat(xjj): 增加内容和markdown样式

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install xu-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add xu-ui</code></pre>
 
-  </div>
+    <p>
+      下一节：<a href="#/doc/get-started">开始使用</a>
+    </p>
+  </article>
 </template>
 
 <script>
