f7187d793d499fe5f20c4eaa962cea96da58c954
徐金俊 <939160741@qq.com>
2021-07-29 16:04:17 +0800

    feat(xjj): index.ts

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -2,3 +2,9 @@ declare module '*.md' {
   const str: string
   export default str
 }
+
+declare module '*.vue' {
+  import {ComponentOptions} from 'vue'
+  const componentOptions: ComponentOptions
+  export default componentOptions
+}
