2fc9f4e04eb70af0d47dec7921291d8b258dea99
Kevin <sxzz@sxzz.moe>
2021-09-15 21:35:46 +0800

    style: format rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,28 @@
 import esbuild from 'rollup-plugin-esbuild'
 import vue from 'rollup-plugin-vue'
 import scss from 'rollup-plugin-scss'
-import dartSass from 'sass';
-import { terser } from "rollup-plugin-terser"
+import dartSass from 'sass'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/lib/index.ts',
-  output: [{
-    globals: {
-      vue: 'Vue'
+  output: [
+    {
+      globals: {
+        vue: 'Vue',
+      },
+      name: 'Moore',
+      file: 'dist/lib/moore.js',
+      format: 'umd',
+      plugins: [terser()],
     },
-    name: 'Moore',
-    file: 'dist/lib/moore.js',
-    format: 'umd',
-    plugins: [terser()]
-  }, {
-    name: 'Moore',
-    file: 'dist/lib/moore.esm.js',
-    format: 'es',
-    plugins: [terser()]
-  }],
+    {
+      name: 'Moore',
+      file: 'dist/lib/moore.esm.js',
+      format: 'es',
+      plugins: [terser()],
+    },
+  ],
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     vue({
@@ -28,7 +31,7 @@ export default {
     esbuild({
       include: /\.[jt]s$/,
       minify: process.env.NODE_ENV === 'production',
-      target: 'es2015'
+      target: 'es2015',
     }),
   ],
 }
